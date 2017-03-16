907d2d689623f760eeef99fde00062f140cf883b
Isaac <hawkins.isaac@gmail.com>
2017-03-16 17:52:40 -0500

    add styles, images, and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,18 @@
 
 
+// Defined variables ===============
 var wordsArray = ["anonymous", "backdoor", "blackhat", "botnet", "ddos", "malware", "phishing", "spoofing", "virus", "whitehat", "attack"];
 
 var randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
 var z = document.getElementById('subject');
+var guessNumber = document.getElementById('guessesLeft');
 var blanks = [];
 
 z.innerHTML = randomWord;
 var s;
+
+//functions ========================
 
 function newWord() {
   for (var i = 0; i < randomWord.length; i++) {
@@ -20,9 +24,60 @@ function newWord() {
 
 newWord();
 
+//when keyboard keys are clicked, log the id value
+var keyboardKeys = document.querySelectorAll('.keyboard-keys');
+var keyNumber = "";
+
+for (var i = 0; i <keyboardKeys.length; i++) {
+	keyboardKeys[i].addEventListener('click', function() {
+		console.log('clicked!');
+		keyNumber = this.getAttribute("id");
+
+    	for (var i = 0; i < randomWord.length; i++) {
+			if (keyNumber === randomWord[i]) {
+				console.log("matched " + keyNumber);
+				this.classList.add('guessed');
+				z.innerHTML = keyNumber;
+
+			}
+}
+
+	})
+}
 
 
-var guessNumber = document.getElementById('guessesLeft');
+document.getElementById('reset-button').addEventListener('click', function() {
+		var randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+
+		var z = document.getElementById('subject');
+		var guessNumber = document.getElementById('guessesLeft');
+		var blanks = [];
+
+		z.innerHTML = randomWord;
+		var s;
+		newWord();
+	});
+
+
+document.getElementById('surrender').addEventListener('click', function() {
+
+		var z = document.getElementById('subject');
+		var guessNumber = document.getElementById('guessesLeft');
+		var blanks = [];
+
+		z.innerHTML = randomWord;
+		var s;
+		randomWord.remove('guessed');
+		newWord();
+	});
+
+// document.getElementById("myBtn").addEventListener("click", function(){
+//     document.getElementById("demo").innerHTML = "Hello World";
+// });
+
+
+
+
 guessNumber.innerHTML = parseInt(randomWord.length / 3 * 2);
 
 
